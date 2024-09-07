f98f50fe7b25b8c2de40213c17bd41b429b79ee2
rohidivvu <112928792+rohidivvu@users.noreply.github.com>
2024-09-07 16:10:45 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "my-ews-zinkle1"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "LockFiles"
     encrypt        = true
   }
 }
